eac6cd7d3e42b4b6e3f79c214153a8c9595e5054
Yury <y-minchuk@mail.ru>
2021-02-24 16:50:06 +0300

    error

--- cmd/less-05/less-05.go
+++ cmd/less-05/less-05.go
@@ -1,40 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"bufio"
 	"os"
-	"strconv"
 )
 
 func main() {
-	arguments := os.Args[1:]
-
-	var numbers []float64
-
-	for _, argument := range arguments {
-		number, err := strconv.ParseFloat(argument, 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		numbers = append(numbers, number)
-	}
-	if maximum(numbers...) != 0 {
-		fmt.Println("Ваш максимальный аргумент: ", maximum(numbers...))
-
-	}
 }
 
-func maximum(numbers ...float64) float64 {
-	if len(numbers) == 0 {
-		fmt.Println("Вы не ввели ни одого аргумента.")
-		return 0
+func getStrings(fileName string) ([]string, error) {
+	var lines []string
+
+	file, err := os.OpenFile(fileName)
+	if err != nil {
+		return nil, err
 	}
-	maxNum := numbers[0]
-	for _, number := range numbers {
-		if maxNum < number {
-			maxNum = number
-		}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
 	}
-	return maxNum
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return lines, nil
 }
