865d33a203325ba190e9c6e7d1504960b7832600
Yury <y-minchuk@mail.ru>
2021-03-07 10:03:47 +0300

    myTree - second varian

--- yury/filelist/main.go
+++ yury/filelist/main.go
@@ -1,4 +1,3 @@
-//Этот код позволяет получить список файлов в директории
 package main
 
 import (
@@ -6,48 +5,48 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
-	"strings"
 )
 
-func formatedPath(path string) string {
-	for i := len(path) - 1; i >= 0; i-- {
-		if path[i] == '/' {
-			// fmt.Println(path[:int(i)])
-			return strings.Repeat(" ", int(i)) + "|_" + path[int(i+1):]
-		}
+func lineToSpace(line string) string {
+	lineSpaces := ""
+	for i := 0; i < len(line); i++ {
+		lineSpaces += " "
 	}
-	return path
+	return lineSpaces + "|_"
 }
 
-//Функция checkList получает название папки и отдает слайс
-//списка файлов и папок в этой папке
 func scanDirectory(path string) error {
+
 	files, err := ioutil.ReadDir(path)
+
 	if err != nil {
-		return err
-	}
-	for _, file := range files {
-		filePath := filepath.Join(path, file.Name())
+		log.Fatal(err)
+	} else {
+		for _, file := range files {
+			filePath := filepath.Join(path, file.Name())
+			length := len(filePath) - len(file.Name())
+			if file.IsDir() {
+				fmt.Printf("%s%s\n", lineToSpace(path), filePath[length:])
+				err := scanDirectory(filePath)
+				if err != nil {
+					return err
+				}
 
-		if file.IsDir() {
-			fmt.Println(formatedPath(filePath))
-			err := scanDirectory(filePath)
+			} else {
+				fmt.Printf("%s%s\n", lineToSpace(path), filePath[length:])
 
-			if err != nil {
-				return err
 			}
-		} else {
-			fmt.Println(formatedPath(filePath))
 		}
-
 	}
 	return nil
 }
 
 func main() {
-	FolderName := "/Users/yury/go/src"
-	err := scanDirectory(FolderName)
+	path := "../"
+	fmt.Printf("%s\n", "|__")
+	err := scanDirectory(path)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
