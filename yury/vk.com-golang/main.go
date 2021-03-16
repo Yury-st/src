aa3e3dcb8758d942b59b8623c0a586943e176940
Yury <y-minchuk@mail.ru>
2021-03-16 17:07:21 +0300

    VK.COM alredy use

--- yury/vk.com-golang/main.go
+++ yury/vk.com-golang/main.go
@@ -7,15 +7,20 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
+//func check(err error) check a mistakes in a err - variable
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func MakeRequest(link string) {
+//func MakeRequest(link string) []byte take a link from our variable and Get respounse
+// to server. If server push request we a bode from request.
+func MakeRequest(link string) []byte {
+
 	resp, err := http.Get(link)
 	check(err)
 
@@ -24,16 +29,11 @@ func MakeRequest(link string) {
 	body, err := ioutil.ReadAll(resp.Body)
 	check(err)
 
-	WriteToFile("request.json", body)
+	return body
 }
 
-type Items struct {
-	response struct {
-		count int
-		items []string
-	}
-}
-
+//func WriteToFile(nameFile string, text []byte)
+//Func write text to file
 func WriteToFile(nameFile string, text []byte) {
 
 	option := os.O_WRONLY | os.O_APPEND | os.O_CREATE
@@ -47,11 +47,28 @@ func WriteToFile(nameFile string, text []byte) {
 	check(err)
 }
 
-func main() {
-	link := "https://api.vk.com/method/wall.get?PARAMETERS&access_token=eb34bd94eb34bd94eb34bd9438eb42630feeb34eb34bd948b79b09179195d7ad35a5a0e&v=5.130&domain=adkazy&count=1&offset="
-	for i := 0; i < 1; i += 1 {
-		MakeRequest(link + strconv.Itoa(i))
-		fmt.Println(link + strconv.Itoa(i))
+//func writeText(textByte string) string take a text and oarcing the text and likes
+// And return clear text. Only text and likes from textByte
+func writeText(textByte string) string {
+	myText := ""
+	textTemplate := "\"text\":"
+	// j := 0
+	for strings.Index(textByte, textTemplate) != -1 {
 
+		indexOfText := strings.Index(textByte, textTemplate)
+		textByte = textByte[indexOfText+len(textTemplate)+1:]
+		i := strings.Index(textByte, "\"")
+		myText += textByte[:i] + "\n" + "===================\n"
+	}
+	fmt.Println(myText)
+	return myText
+}
+
+func main() {
+	var bodyJson []byte
+	link := "https://api.vk.com/method/wall.get?PARAMETERS&access_token=eb34bd94eb34bd94eb34bd9438eb42630feeb34eb34bd948b79b09179195d7ad35a5a0e&v=5.130&domain=adkazy&count=100&offset="
+	for i := 0; i < 2500; i += 100 {
+		bodyJson = MakeRequest(link + strconv.Itoa(i))
+		WriteToFile("request.json", []byte(writeText(string(bodyJson))))
 	}
 }
